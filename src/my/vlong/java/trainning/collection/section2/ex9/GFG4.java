39ecfef16f1fc4436480f5e377be2751ba477de5
Vo Hoang Long <vohoanglong07@gmail.com>
2019-07-28 23:22:43 +0700

    add requirement and checkpoint for workshop02

--- src/my/vlong/java/trainning/collection/section2/ex9/GFG4.java
+++ src/my/vlong/java/trainning/collection/section2/ex9/GFG4.java
@@ -1,7 +1,15 @@
 package my.vlong.java.trainning.collection.section2.ex9;
 
 enum TColor {
-    RED, GREEN, BLUE;
+    RED(1, "Red"), GREEN(2, "Green"), BLUE(3, "Blue");
+
+    private int code;
+    private String name;
+
+    TColor(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
 
     // enum constructor called separately for each
     // constant
